4e59512396a2fc69abf9d89d92479d82f808c8ac
sqizzo <baihaqimnb@gmail.com>
2025-07-02 22:04:56 +0700

    Add like & popular post feature for backend,  prepare for comment system

--- server/Routes/posts.js
+++ server/Routes/posts.js
@@ -4,6 +4,8 @@ const Post = require("../models/Post");
 const router = express.Router();
 const postRoutes = require("../Routes/posts");
 
+// Get all posts
+// url: /posts
 router.get("/", async (req, res) => {
   try {
     const posts = await Post.find().sort({ createdAt: -1 });
@@ -13,6 +15,8 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Create post
+// url: /posts
 router.post("/", async (req, res) => {
   try {
     const { petName, category, imageUrl } = req.body;
@@ -20,6 +24,39 @@ router.post("/", async (req, res) => {
     res.status(201).json(newPost);
   } catch (error) {
     res.status(500).json({ error: "Failed to create post" });
+  }
+});
+
+// Like post
+// url: /posts/:id/like
+router.patch("/:id/like", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const likedPost = await Post.findByIdAndUpdate(
+      id,
+      { $inc: { likes: 1 } },
+      { new: true }
+    );
+
+    if (!likedPost) {
+      res.status(404).json({ error: "Target post was not found" });
+    }
+
+    res.status(200).json(likedPost);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to like the post" });
+  }
+});
+
+// Get 7 most popular posts
+// url: /posts/popular
+router.get("/popular", async (req, res) => {
+  try {
+    const popularPosts = await Post.find().sort({ likes: -1 }).limit(7);
+    res.status(200).json(popularPosts);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch popular posts" });
   }
 });
 
